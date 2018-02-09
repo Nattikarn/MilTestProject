4a3db877e3fa9612972e66ba45f5fa5afc4220e2
Nattikarn <nattikarn@owndays-tech.com>
2018-02-09 11:20:31 +0700

    Created new Project: login UI

---
+++ app/src/main/java/com/example/mil/miltestproject/Test2.java
@@ -0,0 +1,20 @@
+package com.example.mil.miltestproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by mil on 2/9/2018 AD.
+ */
+
+public class Test2 extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test2_activity);
+
+        Intent intent = getIntent();
+
+    }
+}
